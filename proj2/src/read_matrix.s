5819756082335a151079700e1a756c83a15a96bf
BUAA-TYZ <2506741037@qq.com>
2023-11-16 18:06:00 +0800

    FINISH 2/3 of 2B

--- proj2/src/read_matrix.s
+++ proj2/src/read_matrix.s
@@ -27,77 +27,90 @@
 read_matrix:
 
     # Prologue
-    addi sp, sp, -20
-    # s3: file handle
+    addi sp, sp, -28
+    # s3: file handle | s4: malloc address | s5: bytes of malloc
     sw s0, 0(sp)
     sw s1, 4(sp)
     sw s2, 8(sp)
     sw s3, 12(sp)
-    sw ra, 16(sp)
+    sw s4, 16(sp)
+    sw s5, 20(sp)
+    sw ra, 24(sp)
     mv s0, a0
     mv s1, a1
     mv s2, a2
     
     # open the file
-	addi sp, sp, -4
-    sw ra, 0(sp)
     mv a1, s0
     mv a2, x0
     jal fopen
-    ble a0, x0, exception2
+    ble a0, x0, fopen_error
     mv s3, a0
-    lw ra, 0(sp)
-    addi sp, sp, 4
     
     # read row 
-    addi sp, sp, -4
-    sw ra, 0(sp)
     mv a1, s3
     mv a2, s1
     li a3, 4
-    addi sp, sp, -4
-    sw a3, 0(sp)
     jal fread
-    lw a3, 0(sp)
-    addi sp, sp, 4
-    bne a0, a3, exception3
-    lw ra, 0(sp)
-    addi sp, sp, 4
-    
-    # read column
-    addi sp, sp, -4
-    sw ra, 0(sp)   
-    mv a1, s3
-    mv a2, s1
     li a3, 4
-    addi sp, sp, -4
-    sw a3, 0(sp)
-    jal fread
-    lw a3, 0(sp)
-    addi sp, sp, 4
-    bne a0, a3, exception3
-    lw ra, 0(sp)
-    addi sp, sp, 4
+    bne a0, a3, fread_error
     
+    # read column  
+    mv a1, s3
+    mv a2, s2
+    li a3, 4
+    jal fread
+    li a3, 4
+    bne a0, a3, fread_error
+    
+    # malloc
+    lw t1, 0(s1)
+    lw t2, 0(s2)
+    # calculate bytes of malloc
+    mul s5, t1, t2
+    slli s5, s5, 2
+    mv a0, s5
+    jal malloc
+    beq a0, x0, malloc_error
+    mv s4, a0
+    
+    # read matrix
+    mv a1, s3
+    mv a2, s4
+    mv a3, s5
+    jal fread
+    bne a0, s5, fread_error
+    
+    # close file
+    mv a1, s3
+    jal fclose
+    bne a0, x0, fclose_error
 
+    mv a0, s4
     # Epilogue
     lw s0, 0(sp)
-    addi sp, sp, 4
-
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    lw s3, 12(sp)
+    lw s4, 16(sp)
+    lw s5, 20(sp)
+    lw ra, 24(sp)
+    addi sp, sp, 28
+    
     ret
 
-exception1:
+malloc_error:
     li a1, 88
     jal exit2
 
-exception2:
+fopen_error:
     li a1, 90
     jal exit2
     
-exception3:
+fread_error:
     li a1, 91
     jal exit2
 
-exception4:
+fclose_error:
     li a1, 92
     jal exit2
--- proj2/src/write_matrix.s
+++ proj2/src/write_matrix.s
@@ -25,16 +25,75 @@
 write_matrix:
 
     # Prologue
+    addi sp, sp, -28
+    # s4 file handle | s5 #element of matrix
+    sw s0, 0(sp)
+    sw s1, 4(sp)
+    sw s2, 8(sp)
+    sw s3, 12(sp)
+    sw s4, 16(sp)
+    sw s5, 20(sp)
+    sw ra, 24(sp)
 
+    mv s0, a0
+    mv s1, a1
+    mv s2, a2
+    mv s3, a3
+    
+    # open the file
+    mv a1, s0
+    li a2, 1
+    jal fopen
+    ble a0, x0, fopen_error
+    mv s4, a0
 
-
-
-
-
-
-
-
+    # malloc | write row, column
+    li a0, 8
+    jal malloc
+    sw s2, 0(a0)
+    sw s3, 4(a0)
+    mv a1, s4
+    mv a2, a0
+    li a3, 2
+    li a4, 4
+    jal fwrite
+    li a3, 2
+    blt a0, a3, fwrite_error
+    
+    # write matrix
+    mv a1, s4
+    mv a2, s1
+    mul s5, s2, s3
+    mv a3, s5
+    li a4, 4
+    jal fwrite
+    blt a0, s5, fwrite_error
+    
+    # close file
+    mv a1, s4
+    jal fclose
+    bne a0, x0, fclose_error
+    
     # Epilogue
-
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    lw s3, 12(sp)
+    lw s4, 16(sp)
+    lw s5, 20(sp)
+    lw ra, 24(sp)
+    addi sp, sp, 28
 
     ret
+
+fopen_error:
+    li a1, 93
+    jal exit2
+    
+fwrite_error:
+    li a1, 94
+    jal exit2
+    
+fclose_error:
+    li a1, 95
+    jal exit2
