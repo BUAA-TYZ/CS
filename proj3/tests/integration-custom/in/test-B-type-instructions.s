c180c45af7fa8750cabd0c40d7a27b7343af482e
BUAA-TYZ <2506741037@qq.com>
2023-11-29 19:08:12 +0800

    FINISH PROJ3 NON-PIPELINE

---
+++ proj3/tests/integration-custom/in/test-B-type-instructions.s
@@ -0,0 +1,6 @@
+addi s1, x0, 12
+beq s1, s1, label1
+addi s1, x0, 12
+addi s1, x0, 12
+beq  x0, x0, label1
+label1: 
---
+++ proj3/tests/integration-custom/in/test-R-type-Instructions.s
@@ -0,0 +1,11 @@
+addi t0, x0, 1
+add t1, t0, t0
+sub t1, t1, t0
+and t1, t1, t0
+or t1, t1, t0
+mul t1, t1, t0
+
+addi t2, t2, 2
+sll t1, t1, t2
+srl t1, t1, t2
+mulh t1, t1, t2
